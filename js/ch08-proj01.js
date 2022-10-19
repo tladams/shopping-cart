490936b3b35bc520d8bb8d2fb0155c92a87fdf32
Tessa Adams <adams3tl@dukes.jmu.edu>
2022-10-19 16:34:55 -0400

    updated functionality for js files

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -4,3 +4,37 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+
+// first 3 rows
+document.write(outputCartRow("Portrait of Marten Soolmans", 75.0));
+document.write(outputCartRow("View of Houses in Delft", 125.0));
+document.write(outputCartRow("Woman Reading a Letter", 100.0));
+
+// subtotal row
+document.write('<tr>');
+document.write('<td>Subtotal</td>');
+subtotal = calculateSubTotal([75, 125, 100])
+document.write('<td>' + subtotal + '</td>');
+document.write('</tr>');
+
+// tax row
+document.write('<tr>');
+document.write('<td>Tax</td>');
+document.write('<td>' + calculateTax(tax_rate) + '</td>');
+document.write('</tr>');
+
+// shipping row
+document.write('<tr>');
+document.write('<td>Shipping</td>');
+document.write('<td>' + calculateShipping(shipping_threshold)+ '</td>');
+document.write('</tr>');
+
+// grand total row
+document.write('<tr>');
+document.write('<td><strong>Grand Total</strong></td>');
+grandTotal = calculateGrandTotal(subtotal, tax_rate, shipping_threshold);
+document.write('<td>' + '<strong>grandTotal</strong>' + '</td>');
+document.write('</tr>');
+
+
+
--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,58 @@
 /* define your functions here */
 
 
+const itemInfo = ([
+    {
+          product: {
+                title,
+                filename,
+                price
+          },
+          quantity
+    }      
+]) => {
+    itemInfo.map(cart);
+};
+
+function calculateTotal(quantity, price) {
+    return quantity * price;
+}
+
+function outputCartRow(item, total) {
+    document.write('<tr>');
+    document.write('<td>' + itemInfo.filename + '</td>');
+    document.write('<td>' + item + '</td>');
+    document.write('<td>' + itemInfo.quantity + '</td>');
+    document.write('<td>"$' + total.toFixed(2)  + '</td>');
+    document.write('<td>' + calculateTotal(itemInfo.quantity, itemInfo.price).toFixed(2) + '</td>');    
+}
+
+function calculateSubTotal(amounts) {
+    let subtotal = 0;
+    for(let i in amounts) {
+        subtotal += i;
+    }
+    return subtotal;
+}
+
+function calculateGrandTotal(subtotal, tax, ship_thresh) {
+    let shipping = calculateShipping(subtotal, ship_thresh);
+    let grand_total = subtotal + tax + shipping;
+    return grand_total;
+}
+
+function calculateTax(tax) {
+    return tax;
+}
+
+function calculateShipping(subtotal, ship_thresh) {
+    let shipping = 40;
+    if(subtotal > ship_thresh) {
+        shipping = 0;
+    }
+    return shipping;
+}
+
 
 
 
