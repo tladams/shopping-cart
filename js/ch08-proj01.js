89a0c20522b5d42b418317ccfd4c6fa6385615a8
tladams <adams3tl@dukes.jmu.edu>
2022-12-13 11:46:11 -0500

    updated window prompt

--- js/ch08-proj01.js
+++ js/ch08-proj01.js
@@ -1,6 +1,6 @@
 
-const tax_rate = prompt('Enter tax rate (0.10)');
-const shipping_threshold = prompt('Enter shipping threshold (1000)');
+const tax_rate = prompt('Enter tax rate (ex: 0.10)');
+const shipping_threshold = prompt('Enter shipping threshold (ex: 1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
